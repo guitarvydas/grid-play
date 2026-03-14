dc4b2778428376038f485088dd931fd7758b504f
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-03-14 13:59:40 -0400

    remove xcontinues

--- out.py
+++ out.py
@@ -1,6 +1,4 @@
 import rtlib
-import rtlib
-import rtlib
 first="Jane"
 last="Doe"
-rtlib.cellAssign ("A", "1",  "Hello, "  + str(first)  + "!"  + " Your surname is "  + str(last))
+[A1] = "Hello, "  + str(first)  + "!"  + " Your surname is "  + str(last)
